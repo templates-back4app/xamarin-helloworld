35c60b94c1a8c6f40cfb7ed18aa6315a79c14b28
Natália Condê <natalia.csouza98@outlook.com>
2019-06-20 08:56:47 -0300

    first commit - .Net

---
+++ HelloWorldParseServer/HelloWorldParseServer.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Parse;
+
+namespace HelloWorldParseServer.Droid
+{
+    [Activity(Label = "HelloWorldParseServer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            ParseClient.Initialize(new ParseClient.Configuration
+            {
+                ApplicationId = GetString(Resource.String.back4app_app_id),
+                WindowsKey = GetString(Resource.String.back4app_dotnet_key),
+                Server = GetString(Resource.String.back4app_server_url)
+            });
+
+            ParsePush.ParsePushNotificationReceived += ParsePush.DefaultParsePushNotificationReceivedHandler;
+            ParseInstallation.CurrentInstallation["GCMSenderId"] = "754373890304";
+            ParseInstallation.CurrentInstallation.SaveAsync();
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# xamarin-helloworld
+
+In this repository you will find an example app working with Back4App + Javascript SDK.
+
+### Step 1 - Back4App
+
+If it is your first time with Back4App, create a account.
+
+### Step 1.1 - Replace Keys
+
+Don't forget to paste your Back4App App ID and Javascript Key in the `./HelloWorldParseServer/HelloWorldParseServer.Android/Resources/value/styles.xml` file.
+
+## Ready! You can test your application now :)
